92a41761e59ae5d3bb36bbf4b281aa48e9764928
jallend1 <jallend1@gmail.com>
2020-03-17 11:35:11 -0700

    Adds Spock images

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const cells = document.querySelectorAll('.cell');
 const gameInfo = document.querySelector('#gameinfo');
+let quinto = '<img src="quinto.png">';
+let nimoy = '<img src="nimoy.png">';
 let isX = false;
 
 cells.forEach(cell => cell.addEventListener('click', (e) => {
     isX = !isX;
-    isX ? e.target.innerText = "x" : e.target.innerText = 'o';
+    isX ? e.target.innerHTML = quinto : e.target.innerHTML = nimoy;
     gameInfo.innerHTML = `<p>${isX}</p>`;
 }));
